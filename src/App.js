43ad1c1ccb2748c0ae469d0bdd0260dd7638c7df
Gerson <gerson.junior1986@gmail.com>
2021-12-27 23:24:40 -0300

    final Curso Ciclo de vida dos componentes

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React,{Component} from "react";
+import ListaDeNotas from "./Components/ListaDeNotas";
+import FormularioCadastro from "./Components/FormularioCadastro";
+import ListaDeCategorias from "./Components/ListaDeCategorias";
+import "./Assets/App.css";
+import "./Assets/index.css";
+import Categorias from "./dados/Categorias";
+import ArrayDeNotas from "./dados/Notas";
+
+
+class App extends Component {
+
+  constructor(){
+    super();
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
+    
+  }
+  
+ 
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro 
+        categorias={this.categorias}
+        criarNota={this.notas.adicionarNota.bind(this.notas)}/>
+        <main className="conteudo-principal">
+          <ListaDeCategorias 
+          adicionarCategoria={this.categorias.adicionarCategoria.bind(this.categorias)}
+          categorias={this.categorias}/>
+
+        <ListaDeNotas 
+        apagarNota={this.notas.apagarNota.bind(this.notas)}
+        notas={this.notas}/>
+        </main>
+      </section>
+    );
+  }
+}
+export default App;
